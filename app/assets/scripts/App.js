2176f5a445f6aa63da892d45f5d8bad6b81784d2
Will <wfoster12@gmail.com>
2018-07-18 08:56:07 +0100

    added modal with JS and CSS

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,8 +2,10 @@ import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import $ from'jquery';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($('.features__grid-item'), '85%');
 new RevealOnScroll($('.testimonials__card'), '80%');
 var stickyHeader = new StickyHeader();
+var modal = new Modal();
